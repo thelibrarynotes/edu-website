969292703f5220af9c35d95df3717cf23d7512ce
thelibrarynotes <thelibrarynotes@gmail.com>
2026-01-31 19:28:01 +0545

    Add files via upload

---
+++ education-site/script.js
@@ -0,0 +1,48 @@
+/* BUTTON GROUP */
+.btn - group {
+    display: flex;
+    flex - direction: column;
+    gap: 10 px;
+    margin - top: 15 px;
+}
+
+.btn {
+    padding: 10 px;
+    border: none;
+    color: white;
+    cursor: pointer;
+    border - radius: 6 px;
+    transition: all 0.3 s ease;
+    font - size: 14 px;
+}
+
+/* Button colors */
+.view {
+    background: #2563eb;
+
+}
+
+
+
+.download {
+
+  background: # 16 a34a;
+}
+
+.share {
+    background: #9333ea;
+
+}
+
+
+
+/* Click animation */
+
+.btn.clicked {
+
+  background: # facc15!important;
+    color: #000;
+
+  transform: scale(0.95);
+
+}
